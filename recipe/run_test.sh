4bc7881bbf2575f861593371a833417b7598c4f4
Ray Donnelly <mingw.android@gmail.com>
2018-10-29 16:18:31 -0700

    xc-avoidance

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,8 +2,10 @@
 
 cd test
 ln -s ${GXX} g++
-cp ../xcrun .
-cp ../xcodebuild .
+if [[ $(uname) == Darwin ]]; then
+  export PATH=${PREFIX}/xc-avoidance/bin
+  export CONDA_BUILD_SYSROOT=/opt/MacOSX10.9.sdk
+fi
 export PATH=${PWD}:${PATH}
 qmake hello.pro
 make
